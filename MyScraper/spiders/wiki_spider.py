f7ea2021d0fb0c9adc6f764ebab42581fd682d43
stephenysh <stephen.yushihang@gmail.com>
2020-02-05 10:41:48 +0400

    regex add bracket

--- MyScraper/spiders/wiki_spider.py
+++ MyScraper/spiders/wiki_spider.py
@@ -16,6 +16,7 @@ _re_ital = re.compile(r"\_([^\_]+)\_")
 _re_ref = re.compile(r"\[\d+\]")
 _re_img = re.compile(r"\*\[\!\[[^\[]+\]\([^\(]+\)\]\:")
 _re_start_bracket = re.compile(r"\*\[[^\[]+\]\:")
+_re_bracket = re.compile(r"\([^\(]+\)")
 
 def regex_process(text):
     text = re.sub(_re_multi_space, " ", text)
@@ -24,6 +25,7 @@ def regex_process(text):
     text = re.sub(_re_ref, "", text)
     text = re.sub(_re_img, "", text) # img should before start bracket
     text = re.sub(_re_start_bracket, "", text)
+    text = re.sub(_re_bracket, "", text)
     return text
 
 class WikiSpider(CrawlSpider):
