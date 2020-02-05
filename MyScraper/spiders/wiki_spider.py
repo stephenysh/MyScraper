04be248be1971e8c35363b762df4c4a6aba3b650
stephenysh <stephen.yushihang@gmail.com>
2020-02-05 10:19:59 +0400

    modify regex process to filter something like *[]

--- MyScraper/spiders/wiki_spider.py
+++ MyScraper/spiders/wiki_spider.py
@@ -13,13 +13,17 @@ converter.ignore_links = True
 _re_multi_space = re.compile(r"\s+")
 _re_bold = re.compile(r"\*\*([^\*]+)\*\*")
 _re_ital = re.compile(r"\_([^\_]+)\_")
-_re_ref = re.compile("\[\d+\]")
+_re_ref = re.compile(r"\[\d+\]")
+_re_img = re.compile(r"\*\[\!\[[^\[]+\]\([^\(]+\)\]\:")
+_re_start_bracket = re.compile(r"\*\[[^\[]+\]\:")
 
 def regex_process(text):
     text = re.sub(_re_multi_space, " ", text)
     text = re.sub(_re_bold, lambda match: match.group(1), text)
     text = re.sub(_re_ital, lambda match: match.group(1), text)
     text = re.sub(_re_ref, "", text)
+    text = re.sub(_re_img, "", text) # img should before start bracket
+    text = re.sub(_re_start_bracket, "", text)
     return text
 
 class WikiSpider(CrawlSpider):
